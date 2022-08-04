ce8d76b3b2d6aa61e811a2a6e3f68ddc436617d6
Tory <76063134+OsatoVicTory@users.noreply.github.com>
2022-08-04 11:38:49 +0100

    Update loginController.js

--- controller/loginController.js
+++ controller/loginController.js
@@ -32,7 +32,7 @@ exports.loginUser = async (req, res) => {
         })
 
         res.status(200).json({ 
-            message: "User Registered Successfully",
+            message: "User Logged In Successfully",
             token
         })
     } catch (err) {
